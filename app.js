9b011ca0fba5e3205db87e2d3527d6c7194fa384
ErnestoXG <ggoku02@gmail.com>
2022-02-12 00:12:34 +0000

    tests finished

---
+++ app.js
@@ -0,0 +1,27 @@
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    // convert the given valueInEuro to dollars
+    let valueInYen = valueInDollar * 115.60;
+    //return the dollar value
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen){
+    // convert the given valueInEuro to dollars
+    let valueInPound = valueInYen * 0.006384;
+    //return the dollar value
+    return valueInPound;
+}
+
+const sum = (a,b) =>{
+    return a + b
+}
+console.log(sum(7,3));
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
---
+++ test.js
@@ -0,0 +1,23 @@
+const {fromEuroToDollar, fromDollarToYen, fromYenToPound} = require('./app.js')
+
+test('One euro should be 1.206 dollars', () => {
+
+    const dollars = fromEuroToDollar(3.5)
+    const expected = 3.5*1.2
+
+    expect(dollars).toBe(expected)
+})
+
+test('One dollar should be 115.60 yens', ()=>{
+    const yens = fromDollarToYen(3.5)
+    const expected = 3.5 * 115.60
+
+    expect(yens).toBe(expected)
+})
+
+test('One yen should be 0.006384 pounds', ()=>{
+    const pounds = fromYenToPound(35)
+    const expected = 35 * 0.006384
+
+    expect(pounds).toBe(expected)
+})
